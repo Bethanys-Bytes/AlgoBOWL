f4a3253ebaec52f1bc6224e8c7ddbf04e7793874
NickFryMines <nick_fry@mines.edu>
2026-04-09 13:29:47 -0600

    updated 0.in

--- graph_solver.py
+++ graph_solver.py
@@ -2,7 +2,6 @@ from utils.input_parser import FieldTiles, get_point_value, HorseField
 from collections import deque
 from argparse import ArgumentParser
 
-
 class Tile:
     def __init__(self, tile_type: FieldTiles, row, col):
         self.tile_type = tile_type
@@ -28,19 +27,26 @@ class SubField:
     def __init__(self, current_state: set[Tile], boundary: set[Tile] = None):
         self.state = current_state
         self.points = None
-        if boundary:
+        self._hash = None
+        self.boundary: set[Tile] = set()
+        if boundary is None:
+            self.validate_boundary()
+        else:
             self.boundary = boundary
-            return
+
+    def validate_boundary(self):
         self.boundary: set[Tile] = set()
         for tile in self.state:
             outside_state = tile.adjacencies - self.state
             if len(outside_state) > 0:
                 self.boundary |= outside_state
 
-    def include(self, include_tile: Tile, sink_tile: Tile) -> bool:
+    def include(self, include_tile: Tile, sink_tile: Tile):
         if include_tile not in self.boundary:
             print("INCLUDE ERROR")
             return
+        added_to_state: set[Tile] = set()
+        added_to_boundary: set[Tile] = set()
         stack = [include_tile]
         while stack:
             current = stack.pop()
@@ -49,33 +55,35 @@ class SubField:
                 continue
             # Add to current state
             self.state.add(current)
+            added_to_state.add(current)
             # Add adjacencies to new element to boundary
             new_boundary = current.adjacencies - self.state
+            added_to_boundary |= new_boundary
             if sink_tile in new_boundary:
-                return False
+                return (added_to_state, added_to_boundary)
             self.boundary |= new_boundary
             self.boundary.remove(current)
             for next_tile in new_boundary:
                 # If next_tile is not grass, cannot place a wall and more expansion is needed
                 if next_tile.tile_type != FieldTiles.GRASS:
                     stack.append(next_tile)
-                # TODO: If consuming the extended boundary tile does not increase the number of boundary tiles, consume
-                # boundary_adjacent = next_tile.adjacencies - self.state
-                # # If the boundary tile is adjacent to only one unknown tile
-                # # and the following tile
-                # if len(boundary_adjacent) == 1 and next(iter(boundary_adjacent)).tile_type == FieldTiles.GRASS:
-                #     stack.append(next_tile)
-        return True
+                # If consuming the extended boundary tile does not increase the number of boundary tiles, consume
+                boundary_adjacent = next_tile.adjacencies - self.state
+                # If the boundary tile is adjacent to only one unknown tile
+                if len(boundary_adjacent) == 1:
+                    adjacent_next = next(iter(boundary_adjacent))
+                    # If the tile the boundary is adjacent to is not the sink and is grass
+                    if sink_tile != adjacent_next and next(iter(boundary_adjacent)).tile_type == FieldTiles.GRASS:
+                        stack.append(next_tile)
+        return (added_to_state, added_to_boundary)
+
+    def include_precompute(self, added_to_state, added_to_boundary):
+        self.state |= added_to_state
+        self.boundary |= added_to_boundary
+        self.validate_boundary()
 
     def next_possible_expansions(self) -> int:
         return len(self.boundary)
-
-    def is_subset(self, *others: "SubField"):
-        for other in others:
-            if self.state.issubset(other.state):
-                if len(self.boundary - other.state) == 1:
-                    return True
-        return False
 
     def get_points(self):
         if self.points == None:
@@ -139,39 +147,40 @@ if __name__ == "__main__":
         tile_grid[portal2_row][portal2_col].add_adjacency(tile_grid[portal1_row][portal1_col])
 
     subfields: set[SubField] = set()
-    # valid_solutions: set[SubField] = set()
+    boundary_expanded: dict[Tile:tuple[set[Tile],set[Tile]]] = {}
     expand_queue: deque[SubField] = deque()
     initial_state = SubField({tile_grid[field_input.horse[0]][field_input.horse[1]]})
     best_solution: SubField = initial_state
     expand_queue.append(initial_state)
     subfields.add(initial_state)
-    # if initial_state.next_possible_expansions() <= field_input.wallBudget:
-    #     valid_solutions.add(initial_state)
     expansions = 0
     duplicates = 0 #TODO: Minimize duplicates
 
     while len(expand_queue) > 0:
         expand_from = expand_queue.popleft()
-        # if expand_from.is_subset(*subfields): # TODO: Is this valid?
-        #     continue
-        # field_input.display([(wall_tile.row, wall_tile.col) for wall_tile in expand_from.boundary])
         for boundary_tile in expand_from.boundary:
-            expansions += 1
+            if frozenset(expand_from.state | {boundary_tile}) in subfields:
+                continue
             expanded = SubField(set(expand_from.state), set(expand_from.boundary))
-            if not expanded.include(boundary_tile, sink_tile):
-                continue # Horse can escape if expanded here
-            # field_input.display([(wall_tile.row, wall_tile.col) for wall_tile in expanded.boundary])
+            if boundary_tile in boundary_expanded:
+                if sink_tile in boundary_expanded[boundary_tile][1]:
+                    continue # Horse can escape if expanded here
+                expansions += 1
+                expanded.include_precompute(*boundary_expanded[boundary_tile])
+            else:
+                expansions += 1
+                boundary_expanded[boundary_tile] = expanded.include(boundary_tile, sink_tile)
+                if sink_tile in boundary_expanded[boundary_tile][1]:
+                    continue # Horse can escape if expanded here
             if expanded in subfields:
-                # print("DUP")
                 duplicates += 1
                 continue
             subfields.add(expanded)
             expand_queue.append(expanded)
             if expanded.next_possible_expansions() > field_input.wallBudget:
                 continue
-            if all([tile.tile_type == FieldTiles.GRASS for tile in expanded.boundary]):
-                # valid_solutions.add(expanded)
-                if expanded.get_points() > best_solution.get_points():
+            if all(tile.tile_type == FieldTiles.GRASS for tile in expanded.boundary):
+                if best_solution is None or expanded.get_points() > best_solution.get_points():
                     best_solution = expanded
                     if args.filename_out:
                         with open(args.filename_out, "w", encoding="utf-8") as f:
@@ -179,13 +188,5 @@ if __name__ == "__main__":
                     else:
                         file_record(lambda line : print(line), tile_grid, best_solution)
                         print()
-    # print("Expansions: ", expansions)
-    # print("Duplicates: ", duplicates)
-    # Display possible valid outputs
-    # TODO: Verify that this gets ALL possible solutions
-    # for subfield in valid_solutions:
-    #     field_input.display([(wall_tile.row, wall_tile.col) for wall_tile in subfield.boundary])
-    #     print()
-    #     subfield.display()
-    print(best_solution.get_points())
-    field_input.display([(wall_tile.row, wall_tile.col) for wall_tile in best_solution.boundary])
+    print("Expansions: ", expansions)
+    print("Duplicates: ", duplicates)
