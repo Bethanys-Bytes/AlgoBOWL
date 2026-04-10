5c33342f2744c2082fbde04bcf466ed9ea545003
NickFryMines <nick_fry@mines.edu>
2026-04-09 20:20:39 -0600

    fixes

--- outChecker.py
+++ outChecker.py
@@ -15,10 +15,7 @@ for filename in os.listdir(output_dir):
 
     # extract the group number
     output_path = os.path.join(output_dir, filename)
-    if filename.startswith("output"):
-        group_number = filename.replace(".txt", "")
-    else:
-        group_number = filename.replace("LARGE", "").replace("output_group", "").replace("SMALL", "").replace("_manual", "").replace(".txt", "")
+    group_number = filename.replace("LARGE", "").replace("output_group", "").replace("SMALL", "").replace("_manual", "").replace(".txt", "")
 
     input_file = ""
     for file in os.listdir(input_dir):
